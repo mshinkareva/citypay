0d656d28fd2b20cc43f045a746bb887924e4dd99
topal <tomshapavel@yandex.ru>
2016-03-12 16:48:25 +0300

    added config.json description

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var Connection = require('./connection');
 var util = require('util');
+var config = require('./config.json');
 
 var connection = new Connection(util.format('mongodb://%s:%s/%s', config.host, config.port, config.dbname), {
     autoClose: true,
