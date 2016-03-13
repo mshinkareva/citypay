27b847f328cd113dce8327b3c9c9a5249cf16469
topal <tomshapavel@yandex.ru>
2016-03-13 04:11:06 +0300

    added history controller

--- Controllers/history.js
+++ Controllers/history.js
@@ -1,13 +1,41 @@
 var mainDir = require('path').dirname(process.mainModule.filename);
 var models = require(mainDir + '/models');
+var async = require('async');
 
-function getHistory (cb) {
+function getHistoryCollection (cb) {
     models.getConnection(function (err, db) {
         if (err) return cb(err);
         return cb(null, db.collection('history'));
     });
 }
 
+function addHistory(userId, str, cb) {
+    async.waterfall([
+        function (callback) {
+            getHistoryCollection(callback);
+        },
+        function (historyCollection, callback) {
+            historyCollection.insertOne({
+                userId: userId,
+                text: str
+            }, callback);
+        }
+    ], cb);
+}
+
+function getHistory(userId, cb) {
+    async.waterfall([
+        function (callback) {
+            getHistoryCollection(callback);
+        },
+        function (historyCollection, callback) {
+            historyCollection.find({ userId: userId }, callback);
+        }
+    ], cb);
+}
+
 module.exports = {
-    getHistoryCollection: getHistory
+    getHistoryCollection: getHistoryCollection,
+    addHistory: addHistory,
+    getHistory: getHistory
 };
