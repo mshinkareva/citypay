325b04938b6be515b88345d213724274c0563ada
mshinkareva <Hellod0m!>
2016-11-06 21:35:11 +0300

    mongo config redirect

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 var Connection = require('./connection');
 var util = require('util');
-var config = require('./config.json');
+var config = require('../config.json').mongo;
 
-var connection = new Connection(util.format('mongodb://%s:%s/%s', config.host, config.port, config.dbname), {
+var connection = new Connection(util.format('mongodb://%s:%s/%s', config.host, config.port, config.db), {
     autoClose: true,
     closeTimeout: 10
 });
