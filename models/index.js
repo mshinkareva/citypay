2d216488319ebc2cfd7c94dfd00ddee2eaa73638
Aleksandr Shinkarev <a_shinkarev@wargaming.net>
2016-12-26 23:39:13 +0300

    Autoreconnect to db

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ import mongo from 'mongodb'
 import monk from 'monk'
 import config from '../config'
 
-const db = monk(`${config.mongo.host}:${config.mongo.port}/${config.mongo.db}`);
+const db = monk(`${config.mongo.host}:${config.mongo.port}/${config.mongo.db}`, {auto_reconnect: true});
 
 export const users = db.get('users');
 export const auth = db.get('auth');
